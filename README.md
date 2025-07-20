d7eef4ef8ae785656d958478fe658ed3e514824b
LuckShiba <LuckShiba@protonmail.com>
2025-07-19 21:22:14 -0300

    add: logo and text

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Finnalyze.org
+
+Website code developed by <a href="https://github.com/LuckShiba">LuckShiba</a>.
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'Finnalyze';
-export const SITE_DESCRIPTION = 'Hello, world!';
+export const SITE_DESCRIPTION = 'Finnalyze.org; run by an independant, passionate individual with a strive for accurate reporting and in-depth analysis. Topics may range from politics to current obsessions. No topic is off-limits.';
 export const SITE_COLOR = "#ff8000";
